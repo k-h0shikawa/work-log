24216e3d73129043460f751534c1227eacba9683
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2024-02-04 22:56:26 +0900

    CompleteProductListRepositoryのテストをリファクタリング

--- test/product/infrastructure/complete_product_list_repository_test.dart
+++ test/product/infrastructure/complete_product_list_repository_test.dart
@@ -1,21 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:work_log/app/product/infrastructure/complete_product_list_repository.dart';
 
-import '../../test_database_helper.dart';
+import '../../database/test_database_helper.dart';
 
-Future<void> main() async {
-  final database = await TestDatabaseHelper.instance.database;
-  final repository = CompleteProductListRepository(database);
-  test('完了済み商品の一覧取得', () async {
-    final completeProductList = await repository.fetchCompleteProductList();
+void main() {
+  group('CompleteProductListRepository', () {
+    late CompleteProductListRepository repository;
 
-    expect(completeProductList.length, 1);
-  });
-  test('すべての要素のisCompletedが1であること', () async {
-    final completeProductList = await repository.fetchCompleteProductList();
+    setUp(() async {
+      final database = await TestDatabaseHelper.instance.database;
+      repository = CompleteProductListRepository(database);
+    });
 
-    for (final product in completeProductList) {
-      expect(product.isCompleted, 1);
-    }
+    test('fetchCompleteProductListで取得する件数確認', () async {
+      final completeProductList = await repository.fetchCompleteProductList();
+
+      expect(completeProductList.length, 1);
+    });
+
+    test('取得したすべての商品が完了状態であること', () async {
+      final completeProductList = await repository.fetchCompleteProductList();
+
+      for (final product in completeProductList) {
+        expect(product.isCompleted, 1);
+      }
+    });
   });
 }
