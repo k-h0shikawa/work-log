de73158231ece84e04c8b78a84e9c4de0fcb8428
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2024-02-19 22:44:35 +0900

    可能性1一旦コミット

--- lib/app/work/application/state/selected_product_id_notifier.dart
+++ lib/app/work/application/state/selected_product_id_notifier.dart
@@ -4,8 +4,8 @@ part 'selected_product_id_notifier.g.dart';
 @Riverpod()
 class SelectedProductIdNotifier extends _$SelectedProductIdNotifier {
   @override
-  int build(int index) {
-    return 3;
+  int build(int index, {int? productId}) {
+    return productId!;
   }
 
   void updateState(int productId) {
